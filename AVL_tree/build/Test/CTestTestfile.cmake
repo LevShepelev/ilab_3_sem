bef9fe7e6794241287c2c96c8c377830d4d478a2
lev <Shepelev.lv@phystech.edu>
2022-03-01 14:08:47 +0300

    Ok

--- AVL_tree/build/Test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lev/ilab_3_sem/AVL_tree/Google_test_rep
-# Build directory: /home/lev/ilab_3_sem/AVL_tree/build/Test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- AVL_tree/build/Test/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lev/ilab_3_sem/AVL_tree/Google_test_rep/googletest
-# Build directory: /home/lev/ilab_3_sem/AVL_tree/build/Test/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
