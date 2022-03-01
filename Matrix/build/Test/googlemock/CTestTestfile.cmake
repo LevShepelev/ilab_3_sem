2dc7b95083cf9812deeba683644a3b005bd94105
lev <Shepelev.lv@phystech.edu>
2022-03-01 14:04:40 +0300

    oK

--- Matrix/build/Test/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lev/ilab_3_sem/Test/googlemock
-# Build directory: /home/lev/ilab_3_sem/Matrix/build/Test/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
